45f4099384022a334572ce27db9e7516609bafce
Zaven Muradyan <zaven@skepsi.me>
2014-06-20 23:35:11 -0700

    Add more tests for builtins

--- test/builtins.c
+++ test/builtins.c
@@ -174,10 +174,110 @@ void test_builtin_equality() {
     teardown_test(e);
 }
 
+void test_builtin_head() {
+    awlenv* e = setup_test();
+
+    AWL_ASSERT_TYPE(e, "(head 5)", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(head {})", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(head {1 2 3} {4 5 6})", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(head {x y z})", AWLVAL_ERR);
+
+    AWL_ASSERT_CHAINED(e, "(head {1 2 3})",
+            AWL_IASSERT(v->type == AWLVAL_INT)
+            AWL_IASSERT(v->lng == 1L));
+
+    AWL_ASSERT_TYPE(e, "(head {{1 2} y z})", AWLVAL_QEXPR);
+
+    teardown_test(e);
+}
+
+void test_builtin_tail() {
+    awlenv* e = setup_test();
+
+    AWL_ASSERT_TYPE(e, "(tail 20)", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(tail {})", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(tail {1 2} {3 4})", AWLVAL_ERR);
+
+    AWL_ASSERT_CHAINED(e, "(tail {1 2 3 4})",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 3));
+
+    AWL_ASSERT_CHAINED(e, "(tail {1})",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 0));
+
+    teardown_test(e);
+}
+
+void test_builtin_first() {
+    awlenv* e = setup_test();
+
+    AWL_ASSERT_TYPE(e, "(first 20)", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(first {})", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(first {1 2} {3 4})", AWLVAL_ERR);
+
+    AWL_ASSERT_CHAINED(e, "(first {1 2 3 4})",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 1)
+            AWL_IASSERT(v->cell[0]->type == AWLVAL_INT)
+            AWL_IASSERT(v->cell[0]->lng == 1L));
+
+    AWL_ASSERT_CHAINED(e, "(first {1})",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 1)
+            AWL_IASSERT(v->cell[0]->type == AWLVAL_INT)
+            AWL_IASSERT(v->cell[0]->lng == 1L));
+
+    teardown_test(e);
+}
+
+void test_builtin_last() {
+    awlenv* e = setup_test();
+
+    AWL_ASSERT_TYPE(e, "(last 20)", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(last {})", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(last {1 2} {3 4})", AWLVAL_ERR);
+
+    AWL_ASSERT_CHAINED(e, "(last {1 2 3 4})",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 1)
+            AWL_IASSERT(v->cell[0]->type == AWLVAL_INT)
+            AWL_IASSERT(v->cell[0]->lng == 4L));
+
+    AWL_ASSERT_CHAINED(e, "(last {1})",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 1)
+            AWL_IASSERT(v->cell[0]->type == AWLVAL_INT)
+            AWL_IASSERT(v->cell[0]->lng == 1L));
+
+    teardown_test(e);
+}
+
+void test_builtin_list() {
+    awlenv* e = setup_test();
+
+    AWL_ASSERT_TYPE(e, "(list)", AWLVAL_QEXPR);
+    AWL_ASSERT_TYPE(e, "(list 20)", AWLVAL_QEXPR);
+    AWL_ASSERT_TYPE(e, "(list 20 13 2)", AWLVAL_QEXPR);
+    AWL_ASSERT_TYPE(e, "(list {})", AWLVAL_QEXPR);
+    AWL_ASSERT_TYPE(e, "(list {1 2} {3 4} 53)", AWLVAL_QEXPR);
+
+    AWL_ASSERT_CHAINED(e, "(list 1 3 24 {21 32} :23)",
+            AWL_IASSERT(v->type == AWLVAL_QEXPR)
+            AWL_IASSERT(v->count == 5));
+
+    teardown_test(e);
+}
+
 void suite_builtin(void) {
     pt_add_test(test_builtin_arithmetic, "Test Arithmetic", "Suite Builtin");
     pt_add_test(test_builtin_div, "Test Div", "Suite Builtin");
     pt_add_test(test_builtin_mod, "Test Modulo", "Suite Builtin");
     pt_add_test(test_builtin_pow, "Test Pow", "Suite Builtin");
     pt_add_test(test_builtin_equality, "Test Equality", "Suite Builtin");
+    pt_add_test(test_builtin_head, "Test Head", "Suite Builtin");
+    pt_add_test(test_builtin_tail, "Test Tail", "Suite Builtin");
+    pt_add_test(test_builtin_first, "Test First", "Suite Builtin");
+    pt_add_test(test_builtin_last, "Test Last", "Suite Builtin");
+    pt_add_test(test_builtin_list, "Test List", "Suite Builtin");
 }
