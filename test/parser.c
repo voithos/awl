689b031cf34f16b761b8f179e046992f8429ecb0
Zaven Muradyan <zaven@skepsi.me>
2014-06-26 22:01:27 -0700

    Expand parser tests for EExprs

--- test/parser.c
+++ test/parser.c
@@ -52,6 +52,7 @@ void test_parser_eexpr(void) {
     awlenv* e = setup_test();
 
     // EExprs may only appear within QExprs
+    AWL_ASSERT_TYPE(e, "{x y \\(+ 2 4)}", AWLVAL_QEXPR);
     AWL_ASSERT_TYPE(e, "\\(+ 2 3)", AWLVAL_ERR);
     AWL_ASSERT_TYPE(e, "(+ \\(+ 2 3) 5)", AWLVAL_ERR);
 
@@ -62,8 +63,9 @@ void test_parser_cexpr(void) {
     awlenv* e = setup_test();
 
     // CExprs may only appear within QExprs
-    AWL_ASSERT_TYPE(e, "@(+ 2 3)", AWLVAL_ERR);
-    AWL_ASSERT_TYPE(e, "(+ @(+ 2 3) 5)", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "{x y @(first {1 2 3})}", AWLVAL_QEXPR);
+    AWL_ASSERT_TYPE(e, "@(first {1 2 3})", AWLVAL_ERR);
+    AWL_ASSERT_TYPE(e, "(+ @(first {1 2 3}) 5)", AWLVAL_ERR);
 
     teardown_test(e);
 }
