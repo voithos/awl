6e73ea75d65eb9eba0b83cb12b2c30c0743ad4e5
Zaven Muradyan <zaven@skepsi.me>
2014-08-13 09:28:04 -0700

    Update workerawl.js

--- javascripts/workerawl.js
+++ javascripts/workerawl.js
@@ -55,57 +55,57 @@ worker.beginListening();
 
 importScripts('awl.js');
 
+function initialize() {
+    var awl = initializeAwl();
+    worker.initializeWorker(awl);
+
+    /**
+     * Initialize Awl module bindings and evaluation functions
+     */
+    function initializeAwl() {
+        // Util functions
+        var extractString = function(ptr) {
+            var cstr = [];
+            while (true) {
+                var c = Module.getValue(ptr, 'i8');
+                if (c === 0) {
+                    break;
+                }
+                cstr.push(String.fromCharCode(c));
+                ptr++;
+            }
+            return cstr.join('');
+        };
+
+        // Wrap exported functions
+        var setupAwl = Module.cwrap('setup_awl', null, []);
+        var teardownAwl = Module.cwrap('teardown_awl', null, []);
+        var getAwlVersion = Module.cwrap('get_awl_version', null, []);
+        var registerPrintFn = Module.cwrap('register_print_fn', null, ['number']);
+        var awlenvNewTopLevel = Module.cwrap('awlenv_new_top_level', 'number', []);
+        var evalReplStr = Module.cwrap('eval_repl_str', 'void', ['number', 'string']);
+
+        // Initialize
+        setupAwl();
+        var version = extractString(getAwlVersion());
+        var env = awlenvNewTopLevel();
+
+        // Return awl API
+        return {
+            version: version,
+            eval: function(s) {
+                evalReplStr(env, s);
+            },
+            setupPrintFn: function(fn) {
+                var fnPtr = Runtime.addFunction(function(ptr) {
+                    fn(extractString(ptr));
+                });
+                registerPrintFn(fnPtr);
+            }
+        };
+    }
+}
+
 if (typeof Module !== 'undefined') {
     Module.postRun.push(initialize);
-
-    function initialize() {
-        var awl = initializeAwl();
-        worker.initializeWorker(awl);
-
-        /**
-         * Initialize Awl module bindings and evaluation functions
-         */
-        function initializeAwl() {
-            // Util functions
-            var extractString = function(ptr) {
-                var cstr = [];
-                while (true) {
-                    var c = Module.getValue(ptr, 'i8');
-                    if (c === 0) {
-                        break;
-                    }
-                    cstr.push(String.fromCharCode(c));
-                    ptr++;
-                }
-                return cstr.join('');
-            };
-
-            // Wrap exported functions
-            var setupAwl = Module.cwrap('setup_awl', null, []);
-            var teardownAwl = Module.cwrap('teardown_awl', null, []);
-            var getAwlVersion = Module.cwrap('get_awl_version', null, []);
-            var registerPrintFn = Module.cwrap('register_print_fn', null, ['number']);
-            var awlenvNewTopLevel = Module.cwrap('awlenv_new_top_level', 'number', []);
-            var evalReplStr = Module.cwrap('eval_repl_str', 'void', ['number', 'string']);
-
-            // Initialize
-            setupAwl();
-            var version = extractString(getAwlVersion());
-            var env = awlenvNewTopLevel();
-
-            // Return awl API
-            return {
-                version: version,
-                eval: function(s) {
-                    evalReplStr(env, s);
-                },
-                setupPrintFn: function(fn) {
-                    var fnPtr = Runtime.addFunction(function(ptr) {
-                        fn(extractString(ptr));
-                    });
-                    registerPrintFn(fnPtr);
-                }
-            };
-        }
-    }
 }
